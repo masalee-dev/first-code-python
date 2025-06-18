a6780eba2af6a4c5b70e3028dac8087aa69ce1dc
Haeder Ali <[masaleedesign@gmail.com]>
2025-06-18 08:31:00 +0700

    fundamental basic contruction

---
+++ fundamental1-basic-contruction.py
@@ -0,0 +1,21 @@
+"""
+This is a demo or fake project with Python
+"""
+#Basic contruction Python
+#Sequential: Sequential execution
+print('Hello World')
+print('By Ali')
+print('June, 18 2025')
+print('-' * 8 )
+
+#Braching: Selected execution
+want_fast = True
+if want_fast:
+    print('Just go straight, okay!')
+else:
+    print('Another way')
+
+#Repetition
+number_of_children = 4
+for index_children in range(1, number_of_children+1): #number of repetitions
+    print(f'Hi Kids #{index_children}')
--- main.py
+++
@@ -1,3 +0,0 @@
-"""
-This is a demo or fake project with Python
-"""
