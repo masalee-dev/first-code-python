d19f934d87ea80db1a9f82c4e6d2377cafd43f01
Haeder Ali <[masaleedesign@gmail.com]>
2025-06-21 05:41:26 +0700

    Repeating using while

---
+++ Repeating with while.py
@@ -0,0 +1,11 @@
+watch_video = 10
+print("My bro, tell me about 'Series in netflix'")
+
+number_of_watch_video = 0
+print(f'Numbers of watch video {number_of_watch_video}')
+
+while number_of_watch_video < watch_video :
+    number_of_watch_video = number_of_watch_video + 1
+    print(f"Video's-{number_of_watch_video} , done watching")
+
+print(f"Number of watch video {number_of_watch_video}")
