3a23e96bbdffe856a6cc95f3f3725f04274d24b4
Haeder Ali <[masaleedesign@gmail.com]>
2025-06-24 05:27:35 +0700

    update type data list

---
+++ fundamental-data-type-list.py
@@ -0,0 +1,12 @@
+book = ['Bungkam Suara', 'Aku Bukan Sarjana Kertas', 'Angsa dan Kelelawar', "Alegori 420"]
+print(book) # Variable book view
+
+for t in book:
+    print(t) # For in Process
+
+# View book contents at a specific index
+print(book[0])
+print(book[2])
+
+for i in range(0, len(book)):
+    print(book[i]) # For in with range
