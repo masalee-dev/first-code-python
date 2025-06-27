6da0174fe0114b2ab6c754788322db1a5a08b9f6
Haeder Ali <[masaleedesign@gmail.com]>
2025-06-27 09:22:27 +0700

    update

--- fundamental-type-dictonary2.py
+++ fundamental-type-dictonary2.py
@@ -1,8 +1,22 @@
 user = {
-    'id': 1,
-    'name': 'Haeder Ali',
-    'username': 'Masalee',
-    'email': 'haederalee@gmail.com'
+    'id':1,
+    'name':'Haeder Ali',
+    'username':'masalee',
+    'email':'haederalee@gmail.com',
+    'address': {
+        'street': 'Bogasari',
+        'suite': 'No 17',
+        'city': 'Bogor',
+        'zipcode': '16980',
+    }
 }
-print(user)
-print(user['email'])
+
+print(user['address']['zipcode'])
+
+print('\nChange dict to JSON')
+import json
+result = json.dumps(user)
+print(result)
+
+with open('result.json', 'w') as file:
+    json.dump(user, file)
